6768204bd89856381c2cd5858eda089ed5d30eaf
Pauliiiin <paulin.lachaux@edu.ece.fr>
2024-02-23 21:47:30 +0100

    ajout code opti

--- Sudoku.MYBackTrack.solver/Resources/BackTrackingProfiling.py
+++ Sudoku.MYBackTrack.solver/Resources/BackTrackingProfiling.py
@@ -1,59 +1,59 @@
-from timeit import default_timer
+def find_next_cell_to_fill(grid):
+    # Initialisation des variables
+    min_possibilities = 10  # Initialisation à un nombre supérieur au maximum de possibilités
+    next_cell = None  # Initialisation de la prochaine cellule à remplir à None
+    
+    # Parcours de la grille
+    for i in range(9):
+        for j in range(9):
+            # Vérification si la cellule est vide
+            if grid[i][j] == 0:
+                # Récupération des possibilités pour cette cellule
+                possibilities = get_possible_values(grid, i, j)
+                
+                # Mise à jour de la prochaine cellule si le nombre de possibilités est plus petit
+                if len(possibilities) < min_possibilities:
+                    min_possibilities = len(possibilities)
+                    next_cell = (i, j)
+    
+    return next_cell  # Renvoie la prochaine cellule à remplir
 
-#instance = ((0,0,0,0,9,4,0,3,0),
-#           (0,0,0,5,1,0,0,0,7),
-#           (0,8,9,0,0,0,0,4,0),
-#           (0,0,0,0,0,0,2,0,8),
-#           (0,6,0,2,0,1,0,5,0),
-#           (1,0,2,0,0,0,0,0,0),
-#           (0,7,0,0,0,0,5,2,0),
-#           (9,0,0,0,6,5,0,0,0),
-#           (0,4,0,9,7,0,0,0,0))
 
-def findNextCellToFill(grid, i, j):
-        for x in range(i,9):
-                for y in range(j,9):
-                        if grid[x][y] == 0:
-                                return x,y
-        for x in range(0,9):
-                for y in range(0,9):
-                        if grid[x][y] == 0:
-                                return x,y
-        return -1,-1
+def get_possible_values(grid, row, col):
+    # Récupération des valeurs présentes dans la même ligne
+    row_values = set(grid[row])
+    
+    # Récupération des valeurs présentes dans la même colonne
+    col_values = set(grid[i][col] for i in range(9))
+    
+    # Récupération des valeurs présentes dans la même sous-grille
+    box_values = set(grid[i][j] for i in range(row//3*3, row//3*3+3) for j in range(col//3*3, col//3*3+3))
+    
+    # Retourne l'ensemble des valeurs possibles pour cette cellule
+    return {1, 2, 3, 4, 5, 6, 7, 8, 9} - row_values - col_values - box_values
 
-def isValid(grid, i, j, e):
-        rowOk = all([e != grid[i][x] for x in range(9)])
-        if rowOk:
-                columnOk = all([e != grid[x][j] for x in range(9)])
-                if columnOk:
-                        # finding the top left x,y co-ordinates of the section containing the i,j cell
-                        secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
-                        for x in range(secTopX, secTopX+3):
-                                for y in range(secTopY, secTopY+3):
-                                        if grid[x][y] == e:
-                                                return False
-                        return True
-        return False
 
-def solveSudoku(grid, i=0, j=0):
-        i,j = findNextCellToFill(grid, i, j)
-        if i == -1:
-                return True
-        for e in range(1,10):
-                if isValid(grid,i,j,e):
-                        grid[i][j] = e
-                        if solveSudoku(grid, i, j):
-                                return True
-                        # Undo the current cell for backtracking
-                        grid[i][j] = 0
-        return False
-
-#start = default_timer()
-if(solveSudoku(instance)):
-	#print_grid(instance)
-	r=instance
-else:
-	print ("Aucune solution trouvée")
-
-#execution = default_timer() - start
-#print("Le temps de résolution est de : ", execution, " seconds as a floating point value")
+def solve_sudoku(grid):
+    # Trouver la prochaine cellule à remplir
+    next_cell = find_next_cell_to_fill(grid)
+    
+    # Vérifier si la grille est déjà remplie
+    if not next_cell:
+        return True  # La grille est remplie, retourne True (sudoku résolu)
+    
+    # Récupérer les possibilités pour la prochaine cellule
+    row, col = next_cell
+    possibilities = get_possible_values(grid, row, col)
+    
+    # Essayer chaque possibilité pour la prochaine cellule
+    for num in possibilities:
+        grid[row][col] = num  # Place le nombre dans la cellule
+        
+        # Appel récursif pour remplir le reste de la grille
+        if solve_sudoku(grid):
+            return True  # Si une solution est trouvée, retourne True
+        
+        # Si aucune solution n'est trouvée, réinitialise la cellule et essaie la prochaine possibilité
+        grid[row][col] = 0
+    
+    return False  # Retourne False si aucune solution n'est trouvée pour cette branche de l'arbre de recherche
